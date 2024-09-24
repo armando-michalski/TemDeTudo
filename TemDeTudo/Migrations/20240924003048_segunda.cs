3b5209a04882f14429cf0ebf1ed7f9926a52590a
armando-michalkski <armando.rial.michalski@gmail.com>
2024-09-23 21:47:02 -0300

    Atualizado 2024-09-23

---
+++ TemDeTudo/Migrations/20240924003048_segunda.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TemDeTudo.Migrations
+{
+    public partial class segunda : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
